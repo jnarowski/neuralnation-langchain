b9aaf167f38c4eba4f912185b748c76aa5663123
JP Narowski <jnarowski@gmail.com>
2023-05-18 18:17:09 -0600

    Migrated the response to json

--- hello.py
+++ hello.py
@@ -16,12 +16,12 @@ def hello(place="Berlin"):
     # higher temperature leads to more variation, randomness and creativity
     # temperature between 0.7 and 0.9 is most commonly used if you want to experiment and create many variations quickly
     llm = OpenAI(temperature=0.9)
+    
     # https://python.langchain.com/en/latest/modules/prompts/prompt_templates.html
     prompt = PromptTemplate(
         input_variables=["place"],  # list of variables
         template="What are the 3 best places to eat in {place}?",  # prompt
     )
     question = prompt.format(place=place)
-    # split() is used to split the items into a list. The llm response will look like:
-    # "\n\n1. <first item>.\n\n2. <second item>..."
-    return render_template("hello.html", place=place, answer=llm(question).split("\n\n"))
+
+    return { "place": place, "answer": llm(question).split("\n\n") }
